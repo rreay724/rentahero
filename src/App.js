67bb681aa0a1eced9d3c77672de9c779df7d2a6c
Bobby Reay <rreay724@gmail.com>
2020-10-16 15:32:53 -0400

    changed app to class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Heroes from "./pages/Heroes";
 import SingleHero from "./pages/SingleHero";
 
-function App() {
-  return (
-    <>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/heroes" component={Heroes} />
-        <Route exact path="/heroes/:slug" component={SingleHero} />
-      </Switch>
-    </>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/heroes" component={Heroes} />
+          <Route exact path="/heroes/:slug" component={SingleHero} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
