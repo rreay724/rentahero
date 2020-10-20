2ccfac28ec1319616305988156848646f56961dc
Bobby Reay <rreay724@gmail.com>
2020-10-20 10:48:24 -0400

    reverted everything

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { HeroProvider } from "./rentAHero/context";
+import { HeroProvider } from "./context";
 
 ReactDOM.render(
   <>
